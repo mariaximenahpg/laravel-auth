36d3469b5e0c9479b131758eb6f6a34066a68a1c
mariaximenahpg <federico.businessrico@gmail.com>
2023-01-26 22:20:16 +0100

    modale

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -32,13 +32,34 @@
                 <td>
                   <a href=" {{ route('admin.projects.show', $project) }}" class="btn btn-success"><i class="fa-solid fa-eye"></i></a>
                   <a href=" {{ route('admin.projects.edit', $project) }}" class="btn btn-warning"><i class="fa-solid fa-pen"></i></a>
-                  <form action="{{ route('admin.projects.destroy', $project) }}" class="d-inline-block" method="POST">
-                    @csrf
-                    @method('DELETE')
-                    <button class="btn btn-danger"><i class="fa-solid fa-trash"></i></button>
-                  </form>
+                  <!-- Button trigger modal -->
+                  <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{$project->id}}">
+                    <i class="fa-solid fa-trash"></i>
+                  </button>
                 </td>
-            </tr> 
+            </tr>
+            <!-- Modal -->
+            <div class="modal fade" id="modal-{{$project->id}}" tabindex="-1">
+              <div class="modal-dialog">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Conferma</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                  </div>
+                  <div class="modal-body">
+                    Sei sicuro di voler cancellare il progetto "{{$project->project_name}}"?
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Annulla</button>
+                    <form action="{{ route('admin.projects.destroy', $project) }}" class="d-inline-block" method="POST">
+                      @csrf
+                      @method('DELETE')
+                      <button type="submit" class="btn btn-primary">Continua</button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div> 
             @endforeach
         </tbody>
       </table>
